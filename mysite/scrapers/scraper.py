a95e89bb3791c5de5416864aa56b5c4d7c20044a
Nasiia <anastasia.karavaeva1961@gmail.com>
2025-02-21 04:40:18 +0200

    Developed clean_html method

--- mysite/scrapers/scraper.py
+++ mysite/scrapers/scraper.py
@@ -4,7 +4,7 @@ import os
 from urllib.parse import urlparse
 from typing import Optional
 
-class WebScrapper:
+class WebScraper:
 
     def __init__(self, website_url:str, headers: dict = None, remove_tags=None, remove_styles=None):
 
@@ -13,14 +13,14 @@ class WebScrapper:
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0'
         }
 
-        self.remove_tags = remove_tags or ["script", "style"]
+        self.remove_tags = remove_tags or ["style"]
         self.remove_styles = remove_styles
         
     def get_page(self) -> Optional[requests.Response]:
 
         try:
             response = requests.get(self.website_url, headers=self.headers)
-            response.raise_for_status
+            response.raise_for_status()
             return response
         except requests.exceptions.HTTPError as err:
             print(f"HTTP error occcured: {err}")
@@ -69,45 +69,6 @@ class WebScrapper:
         else:
             print("Failed to retrieve the page")
 
+scraper = WebScraper("https://dom.ria.com/uk/", remove_styles=["display:;"])
+scraper.scrape()
 
-
-
-
-
-# def html_page_scraping(website_url:str) -> None:
-#     url = website_url
-#     headers = {
-#             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0'
-#         }
-#     page = requests.get(url, headers=headers)
-
-#     parsed_url = urlparse(url)
-#     site_name = parsed_url.netloc 
-
-#     site_name = site_name.replace('.', '-')
-
-#     file_name = f"{site_name}.html"
-
-
-#     script_directory = os.path.dirname(os.path.abspath(__file__))
-#     file_path = os.path.join(script_directory, file_name)
-
-#     if page.status_code == 200:
-#         print(f"Success. Status code: {page.status_code}")
-
-#         html_content = page.text
-
-#         soup = BeautifulSoup(page.text, "html.parser")
-#         for tag in soup.find_all(style=True):
-#             if "display:;" in tag["style"]:
-#                 del tag["style"]
-
-
-#         with open(file_path, "w", encoding="utf-8") as file:
-#             file.write(soup.prettify())
-
-#         print(f"HTML saved in {file_name} file")
-
-
-#     else:
-#         print(f"Error. Status code: {page.status_code}")
