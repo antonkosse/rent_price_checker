dd7e65edbc93d892f7ba4d3122e739ff86916c76
Nasiia <anastasia.karavaeva1961@gmail.com>
2025-02-21 03:26:11 +0200

    Implemented ger_page method

--- mysite/scrapers/scraper.py
+++ mysite/scrapers/scraper.py
@@ -2,52 +2,66 @@ from bs4 import BeautifulSoup
 import requests
 import os
 from urllib.parse import urlparse
+from typing import Optional
 
 class WebScrapper:
 
     def __init__(self, website_url:str):
-        self.website_utl = website_url
+        self.website_url = website_url
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0'
         }
         self.page = self.get_page()
     
+    def get_page(self) -> Optional[requests.Response]:
+        try:
+            response = requests.get(self.website_url, headers=self.headers)
+            response.raise_for_status
+            return response
+        except requests.exceptions.HTTPError as err:
+            print(f"HTTP error occcured: {err}")
+            return None
+        except Exception as err:
+            print(f"Other error occcured: {err}")
+            return None
 
 
-def html_page_scraping(website_url:str) -> None:
-    url = website_url
-    headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0'
-        }
-    page = requests.get(url, headers=headers)
-
-    parsed_url = urlparse(url)
-    site_name = parsed_url.netloc 
-
-    site_name = site_name.replace('.', '-')
-
-    file_name = f"{site_name}.html"
 
 
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_directory, file_name)
+# def html_page_scraping(website_url:str) -> None:
+#     url = website_url
+#     headers = {
+#             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0'
+#         }
+#     page = requests.get(url, headers=headers)
 
-    if page.status_code == 200:
-        print(f"Success. Status code: {page.status_code}")
+#     parsed_url = urlparse(url)
+#     site_name = parsed_url.netloc 
 
-        html_content = page.text
+#     site_name = site_name.replace('.', '-')
 
-        soup = BeautifulSoup(page.text, "html.parser")
-        for tag in soup.find_all(style=True):
-            if "display:;" in tag["style"]:
-                del tag["style"]
-
-
-        with open(file_path, "w", encoding="utf-8") as file:
-            file.write(soup.prettify())
-
-        print(f"HTML saved in {file_name} file")
+#     file_name = f"{site_name}.html"
 
 
-    else:
-        print(f"Error. Status code: {page.status_code}")
+#     script_directory = os.path.dirname(os.path.abspath(__file__))
+#     file_path = os.path.join(script_directory, file_name)
+
+#     if page.status_code == 200:
+#         print(f"Success. Status code: {page.status_code}")
+
+#         html_content = page.text
+
+#         soup = BeautifulSoup(page.text, "html.parser")
+#         for tag in soup.find_all(style=True):
+#             if "display:;" in tag["style"]:
+#                 del tag["style"]
+
+
+#         with open(file_path, "w", encoding="utf-8") as file:
+#             file.write(soup.prettify())
+
+#         print(f"HTML saved in {file_name} file")
+
+
+#     else:
+#         print(f"Error. Status code: {page.status_code}")
